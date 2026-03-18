bfcc5e31253004743ad80b10698df72f874cae9b
Menna Walid <mennawalid1244@gmail.com>
2026-03-18 15:31:54 +0200

    Initial project setup with routing and database connection

---
+++ Database.php
@@ -0,0 +1,19 @@
+<?php
+class Database {
+    public $connection;
+
+    public function __construct($config, $username = 'root', $password = '') {
+
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
+
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+
+    public function query($query, $params = []) {
+        $statement = $this->connection->prepare($query);
+        $statement->execute($params);
+        return $statement;
+    }
+}
---
+++ Views/partials/head.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" class=h-full bg-gray-900>
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body class="h-full">
+    <script src="https://cdn.tailwindcss.com"></script> 
+
+<div class="min-h-full">
